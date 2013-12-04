e7de64b118cabe8fae8420dde89d030f8a4e0cd0
Gabriel Pettier <gabriel.pettier@gmail.com>
2013-12-04 01:15:17 +0100

    fix init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ while true
 do
 read -p "do you want to link pentadactylrc to ~/.pentadactylrc (Y/N)" yn
 case $yn in
-	[Yy]*) ln -s ~/.zsh/pentadactylrc ~/.pentadactylrc; break;;
+	[Yy]*) ln -sf $PWD/pentadactylrc ~/.pentadactylrc; break;;
 	[Nn]*) break;;
 	*) echo "please answer yes or no";;
 esac
